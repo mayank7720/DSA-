6140c2b39af0898270f69a968edd69060478f16a
Mayank Raj <mydv986@gmail.com>
2026-04-22 15:02:21 +0530

    Time: 15 ms (64.66%) | Memory: 92.9 MB (69.88%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> unique = new HashSet<>();
+
+        for(int num : nums){
+            if(unique.contains(num)) 
+            {return true;
+            }
+            unique.add(num);
+        }
+
+        return false;
+    }
+}
