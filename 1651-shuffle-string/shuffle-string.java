f3e39b8cadd02a4ba46c945f8ee9e5a56014f6ac
Mayank Raj <mydv986@gmail.com>
2026-04-24 00:51:44 +0530

    Time: 1 ms (66.76%) | Memory: 45.4 MB (74.10%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String restoreString(String s, int[] ind) {
+        char[] r=new char[s.length()];
+        for(int i=0;i<s.length();i++) r[ind[i]]=s.charAt(i);
+        return new String(r);
+    }
+}
